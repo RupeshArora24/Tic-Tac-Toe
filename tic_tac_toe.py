4f98be4a0573ad50d2035edb9f5fdca4c5a91297
RupeshArora24 <rupesh.arora303@gmail.com>
2025-02-16 17:46:39 +0530

    added a input player method to take tehe one player input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,6 +2,7 @@ board = ["-","-","-","-","-","-","-","-","-"]
 you = "X"
 winner = None
 gameRunning = True
+currentPlayer = "X"
 
 def printBoard():  #1 printing the game board 
     print(board[0] + " | " + board[1]+" | "+board[2])
@@ -10,10 +11,18 @@ def printBoard():  #1 printing the game board
     print("--|---|---")
     print(board[6] + " | " + board[7]+" | "+board[8])
 
-if __name__ == "__main__":
-    xState = [0,0,0,0,0,0,0,0]  #2 Lists  
-    OState = [0,0,0,0,0,0,0,0]
-    turn = 1 # 1 for x and 0 for o
-    print("Welcome to Tic Tac Toe")
-    print("X's Chance")
-    printBoard()
+#step 2 take player input 
+def playerInput(board):
+    inp = int(input("Enter a number 1-9 : "))
+    if inp >=1 and inp<=9 and inp== "-": # here we are checking the entered input is valid or not, and is the place is empty to mae the entry
+        board[inp-1]=currentPlayer
+
+
+# Step 3 - check for win or tie
+
+# Step 4 - switch the player
+ 
+# step 5 - check for win or tie again
+while gameRunning:
+    printBoard(board) 
+    playerInput(board)        
